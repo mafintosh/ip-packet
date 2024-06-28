c62fc080a53390c58c59d60fd10e749255982b3c
Mathias Buus <mathiasbuus@gmail.com>
2024-06-28 19:48:20 +0200

    update

--- index.js
+++ index.js
@@ -1,97 +1,93 @@
-module.exports = configure({})
-
-function configure (opts) {
-  function encode (packet, buf, offset) {
-    if (!buf) buf = new Buffer(encodingLength(packet))
-    if (!offset) offset = 0
-
-    buf[offset] = packet.version << 4 | (packet.ihl || 5)
-    buf[offset + 1] = (packet.dscp || 0) << 2 | (packet.ecn || 0)
-    buf.writeUInt16BE(20 + packet.data.length, offset + 2)
-    buf.writeUInt16BE(packet.identification || 0, offset + 4)
-    buf.writeUInt16BE((packet.flags || 0) << 13 | (packet.fragmentOffset || 0), offset + 6)
-    buf[offset + 8] = packet.ttl || 0
-    buf[offset + 9] = packet.protocol || 0
-    buf.writeUInt16BE(0, offset + 10)
-    encodeIp(packet.sourceIp, buf, offset + 12)
-    encodeIp(packet.destinationIp, buf, offset + 16)
-    buf.writeUInt16BE(checksum(buf, offset, offset + 20), offset + 10)
-    packet.data.copy(buf, offset + 20)
-
-    encode.bytes = 20 + packet.data.length
-
-    return buf
-  }
-
-  function decode (buf, offset) {
-    if (!offset) offset = 0
-
-    var version = buf[offset] >> 4
-    if (version !== 4) throw new Error('Currently only IPv4 is supported')
-    var ihl = buf[offset] & 15
-    if (ihl > 5) throw new Error('Currently only IHL <= 5 is supported')
-    var length = buf.readUInt16BE(offset + 2)
-    var decodedChecksum = buf.readUInt16BE(offset + 10)
-    var ignoreChecksum = opts.allowNullChecksum && decodedChecksum === 0
-
-    if (!ignoreChecksum) {
-      var sum = checksum(buf, offset, offset + 20)
-
-      if (sum) throw new Error('Bad checksum (' + sum + ')')
+const ipv4 = {
+  preencode (state, ip) {
+    state.end += 4
+  },
+  encode (state, ip) {
+    for (let i = 0; i < 4; i++) {
+      const oct = parseInt(ip, 10)
+      state.buffer[state.start++] = oct
+      ip = ip.slice(oct < 100 ? oct < 10 ? 2 : 3 : 4)
     }
-
-    decode.bytes = length
-    return {
-      version: version,
-      ihl: ihl,
-      dscp: buf[offset + 1] >> 2,
-      ecn: buf[offset + 1] & 3,
-      length: length,
-      identification: buf.readUInt16BE(offset + 4),
-      flags: buf[offset + 6] >> 5,
-      fragmentOffset: buf.readUInt16BE(offset + 6) & 8191,
-      ttl: buf[offset + 8],
-      protocol: buf[offset + 9],
-      checksum: decodedChecksum,
-      sourceIp: decodeIp(buf, offset + 12),
-      destinationIp: decodeIp(buf, offset + 16),
-      data: buf.slice(offset + 20, offset + length)
-    }
+  },
+  decode (state) {
+    return state.buffer[state.start++] + '.' + state.buffer[state.start++] + '.' + state.buffer[state.start++] + '.' + state.buffer[state.start++]
   }
+}
 
-  function encodingLength (packet) {
-    return 20 + packet.data.length
+const uint16be = {
+  preencode (state, n) {
+    state.end += 2
+  },
+  encode (state, n) {
+    state.buffer[state.start++] = n >>> 8
+    state.buffer[state.start++] = n & 255
+  },
+  decode (state) {
+    return 256 * state.buffer[state.start++] + state.buffer[state.start++]
   }
+}
 
-  encode.bytes = decode.bytes = 0
+exports.preencode = function (state, packet) {
+  state.end += 20 + packet.data.byteLength
+}
+
+exports.encode = function (state, packet) {
+  state.buffer[state.start++] = packet.version << 4 | (packet.ihl || 5)
+  state.buffer[state.start++] = (packet.dscp || 0) << 2 | (packet.ecn || 0)
+  uint16be.encode(state, 20 + packet.data.byteLength)
+  uint16be.encode(state, packet.identification || 0)
+  uint16be.encode(state, (packet.flags || 0) << 13 | (packet.fragmentOffset || 0))
+  state.buffer[state.start++] = packet.ttl || 0
+  state.buffer[state.start++] = packet.protocol || 0
+  uint16be.encode(state, packet.checksum)
+  ipv4.encode(state, packet.sourceIp)
+  ipv4.encode(state, packet.destinationIp)
+  packet.data.copy(state.buffer, state.start)
+  state.start += packet.data.byteLength
+}
+
+exports.decode = function (state) {
+  let n = state.buffer[state.start++]
+
+  const version = n >> 4
+  if (version !== 4) throw new Error('Currently only IPv4 is supported')
+  const ihl = n & 15
+  if (ihl > 5) throw new Error('Currently only IHL <= 5 is supported')
+
+  n = state.buffer[state.start++]
+  const dscp = n >> 2
+  const ecn = n & 3
+
+  const length = uint16be.decode(state)
+  const identification = uint16be.decode(state)
+
+  n = uint16be.decode(state)
+  const flags = n >> 13
+  const fragmentOffset = n & 8191
+
+  const ttl = state.buffer[state.start++]
+  const protocol = state.buffer[state.start++]
+  const checksum = uint16be.decode(state)
+
+  const sourceIp = ipv4.decode(state)
+  const destinationIp = ipv4.decode(state)
+
+  const data = state.buffer.subarray(state.start, state.start += (length - 20))
 
   return {
-    configure: configure,
-    encodingLength: encodingLength,
-    encode: encode,
-    decode: decode
+    version,
+    ihl,
+    dscp,
+    ecn,
+    length,
+    identification,
+    flags,
+    fragmentOffset,
+    ttl,
+    protocol,
+    sourceIp,
+    destinationIp,
+    checksum,
+    data
   }
-}
-
-function encodeIp (addr, buf, offset) {
-  for (var i = 0; i < 4; i++) {
-    var oct = parseInt(addr, 10)
-    buf[offset++] = oct
-    addr = addr.slice(oct < 100 ? oct < 10 ? 2 : 3 : 4)
-  }
-}
-
-function decodeIp (buf, offset) {
-  return buf[offset] + '.' + buf[offset + 1] + '.' + buf[offset + 2] + '.' + buf[offset + 3]
-}
-
-function checksum (buf, offset, end) {
-  var sum = 0
-  for (; offset < end; offset += 2) sum += buf.readUInt16BE(offset)
-  while (true) {
-    var carry = sum >> 16
-    if (!carry) break
-    sum = (sum & 0xffff) + carry
-  }
-  return ~sum & 0xffff
 }
--- test.js
+++ test.js
@@ -1,47 +1,23 @@
-var tape = require('tape')
-var ip = require('./')
+const test = require('brittle')
+const c = require('compact-encoding')
+const ip = require('./')
 
-tape('encode + decode', function (t) {
-  var packet = {
-    version: 4,
-    protocol: 0,
-    sourceIp: '127.0.0.1',
-    destinationIp: '127.0.0.1',
-    data: new Buffer('lol')
+const FIXTURE = Buffer.from('4500004000004000400626510a16001e0a16001efb311f900a7fd88600000000b0c2ffff97830000020404ec010303060101080a9158fbf80000000004020000', 'hex')
+
+test('test tcp packet', function (t) {
+  const packet = c.decode(ip, FIXTURE)
+
+  t.is(packet.sourceIp, '10.22.0.30')
+  t.is(packet.destinationIp, '10.22.0.30')
+
+  const buffer = c.encode(ip, packet)
+
+  for (let i = 0; i < FIXTURE.byteLength; i++) {
+    if (buffer[i] !== FIXTURE[i]) {
+      console.log(i, buffer)
+      process.exit(1)
+    }
   }
 
-  var buf = ip.encode(packet)
-
-  t.same(ip.encodingLength(packet), buf.length)
-
-  var decoded = ip.decode(buf)
-
-  t.same(decoded.version, packet.version)
-  t.same(decoded.protocol, packet.protocol)
-  t.same(decoded.sourceIp, packet.sourceIp)
-  t.same(decoded.destinationIp, packet.destinationIp)
-  t.same(decoded.data, packet.data)
-  t.same(ip.encode.bytes, buf.length)
-  t.same(ip.decode.bytes, buf.length)
-
-  t.end()
-})
-
-tape('decode bad checksum', function (t) {
-  var packet = {
-    version: 4,
-    protocol: 0,
-    sourceIp: '127.0.0.1',
-    destinationIp: '127.0.0.1',
-    data: new Buffer('lol')
-  }
-
-  var buf = ip.encode(packet)
-  buf[4] = 42
-
-  t.throws(function () {
-    ip.decode(buf)
-  })
-
-  t.end()
+  t.alike(buffer, FIXTURE)
 })
